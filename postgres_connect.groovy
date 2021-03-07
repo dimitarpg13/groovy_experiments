2695148a94305b54cc0b926f496459b121b46d71
Dimitar Gueorguiev <dimitar.gueorguiev@celect.com>
2021-03-06 20:31:35 -0500

    postgres connect

---
+++ postgres_connect.groovy
@@ -0,0 +1,13 @@
+import groovy.sql.Sql
+
+@GrabConfig(systemClassLoader=true)
+@Grab(group='org.postgresql', module='postgresql', version='42.2.2')
+
+def dbUrl = "jdbc:postgresql://localhost/test-db"
+def dbUser = "postgres"
+def dbPassword = "postgres"
+def dbDriver = "org.postgresql.Driver"
+
+def sql = Sql.newInstance(dbUrl, dbUser, dbPassword, dbDriver)
+
+sql.eachRow("select * from accounts", { println it.user_id + " -- ${it.username} --"} );
