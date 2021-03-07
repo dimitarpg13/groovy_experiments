f95a15db2c38d934943994dd74e7cf31907303e5
Dimitar Gueorguiev <dimitar.gueorguiev@celect.com>
2021-03-06 20:36:08 -0500

    postgres connect

--- postgres_connect.groovy
+++ postgres_connect.groovy
@@ -11,3 +11,6 @@ def dbDriver = "org.postgresql.Driver"
 def sql = Sql.newInstance(dbUrl, dbUser, dbPassword, dbDriver)
 
 sql.eachRow("select * from accounts", { println it.user_id + " -- ${it.username} --"} );
+
+row = sql.firstRow("select username, email from accounts")
+println "Row: username = ${row.username} and email = ${row.email}"
